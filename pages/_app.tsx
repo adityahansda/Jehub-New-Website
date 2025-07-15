d1913790d0326f9fa0b09b7b5224c135114c8c7d
Aditya Hansda <40536327+adityahansda@users.noreply.github.com>
2025-07-15 14:21:21 +0530

    Coming soon UI and some changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,27 @@ import 'react-pdf/dist/Page/AnnotationLayer.css'
 import 'react-pdf/dist/Page/TextLayer.css'
 import Layout from '../src/components/Layout'
 import { AuthProvider } from '../src/contexts/AuthContext'
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  
+  // Check if coming soon mode is enabled
+  const isComingSoonMode = process.env.NEXT_PUBLIC_COMING_SOON_MODE === 'true'
+  
+  // Don't wrap with Layout if we're in coming soon mode and on the home page,
+  // or if we're directly accessing the coming-soon page
+  const shouldUseLayout = !(isComingSoonMode && router.pathname === '/') && router.pathname !== '/coming-soon'
+  
   return (
     <AuthProvider>
-      <Layout>
+      {shouldUseLayout ? (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      ) : (
         <Component {...pageProps} />
-      </Layout>
+      )}
     </AuthProvider>
   )
 }
