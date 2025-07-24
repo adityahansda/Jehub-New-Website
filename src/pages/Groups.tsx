6234826c3acc1b6d66a5f88da0f3f2d7bf7c1834
Aditya Hansda <40536327+adityahansda@users.noreply.github.com>
2025-07-24 19:51:01 +0530

    page spacing fix

--- src/pages/Groups.tsx
+++ src/pages/Groups.tsx
@@ -60,7 +60,7 @@ const Groups = () => {
   ];
 
   return (
-    <div className="min-h-screen py-8 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen pt-24 pb-8 px-4 sm:px-6 lg:px-8">
       <div className="max-w-7xl mx-auto">
         {/* Header */}
         <div className="text-center mb-12">
